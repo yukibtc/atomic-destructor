8969a6e25d836aab44e55408ba6cd054f97efa5e
Yuki Kishimoto <yukikishimoto@protonmail.com>
2024-05-23 17:26:45 +0200

    Add stealth clone support

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,14 @@ mod saturating;
 
 use self::saturating::SaturatingUsize;
 
+/// Stealth clone
+pub trait StealthClone {
+    /// Clone without increment the atomic destructor counter.
+    ///
+    /// Items that are stealth cloned, NOT decrement the counter when dropped.
+    fn stealth_clone(&self) -> Self;
+}
+
 /// Atomic destroyer
 pub trait AtomicDestroyer: Debug + Clone {
     /// Optional name to identify inner in logs/teminal
@@ -36,6 +44,7 @@ where
 {
     destroyed: Arc<AtomicBool>,
     counter: Arc<AtomicUsize>,
+    stealth: bool,
     inner: T,
 }
 
@@ -67,6 +76,7 @@ where
         f.debug_struct("AtomicDestructor")
             .field("destroyed", &self.destroyed)
             .field("counter", &self.counter)
+            .field("stealth", &self.stealth)
             .field("inner", &self.inner)
             .finish()
     }
@@ -89,6 +99,21 @@ where
         Self {
             destroyed: self.destroyed.clone(),
             counter: self.counter.clone(),
+            stealth: false,
+            inner: self.inner.clone(),
+        }
+    }
+}
+
+impl<T> StealthClone for AtomicDestructor<T>
+where
+    T: AtomicDestroyer,
+{
+    fn stealth_clone(&self) -> Self {
+        Self {
+            destroyed: self.destroyed.clone(),
+            counter: self.counter.clone(),
+            stealth: true,
             inner: self.inner.clone(),
         }
     }
@@ -99,6 +124,13 @@ where
     T: AtomicDestroyer,
 {
     fn drop(&mut self) {
+        if self.is_stealth() {
+            #[cfg(feature = "tracing")]
+            tracing::debug!("Tried to drop stealth destructor, ignore.");
+
+            return;
+        }
+
         if self.is_destroyed() {
             #[cfg(feature = "tracing")]
             if let Some(name) = &self.inner.name() {
@@ -144,12 +176,112 @@ where
         Self {
             destroyed: Arc::new(AtomicBool::new(false)),
             counter: Arc::new(AtomicUsize::new(1)),
+            stealth: false,
             inner,
         }
+    }
+
+    /// Get counter
+    pub fn counter(&self) -> usize {
+        self.counter.load(Ordering::SeqCst)
     }
 
     /// Check if destroyed
     pub fn is_destroyed(&self) -> bool {
         self.destroyed.load(Ordering::SeqCst)
+    }
+
+    /// Check if is stealth (stealth cloned, not subject to counter increase/decrease)
+    pub fn is_stealth(&self) -> bool {
+        self.stealth
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[derive(Debug, Clone)]
+    struct InternalTestingStealth;
+
+    impl AtomicDestroyer for InternalTestingStealth {
+        fn on_destroy(&self) {}
+    }
+
+    #[derive(Clone)]
+    struct TestingStealth {
+        inner: AtomicDestructor<InternalTestingStealth>,
+    }
+
+    impl StealthClone for TestingStealth {
+        fn stealth_clone(&self) -> Self {
+            Self {
+                inner: self.inner.stealth_clone(),
+            }
+        }
+    }
+
+    impl TestingStealth {
+        pub fn new() -> Self {
+            Self {
+                inner: AtomicDestructor::new(InternalTestingStealth),
+            }
+        }
+    }
+
+    #[test]
+    fn test_clone() {
+        let t = TestingStealth::new();
+        assert_eq!(t.inner.counter(), 1);
+        assert!(!t.inner.is_stealth());
+        assert!(!t.inner.is_destroyed());
+
+        let t_1 = t.clone();
+        assert_eq!(t.inner.counter(), 2);
+        assert_eq!(t_1.inner.counter(), 2);
+
+        let t_2 = t_1.clone();
+        assert_eq!(t.inner.counter(), 3);
+        assert_eq!(t_1.inner.counter(), 3);
+        assert_eq!(t_2.inner.counter(), 3);
+
+        drop(t_1);
+        assert_eq!(t.inner.counter(), 2);
+        assert!(!t.inner.is_destroyed());
+
+        drop(t_2);
+        assert_eq!(t.inner.counter(), 1);
+    }
+
+    #[test]
+    fn test_stealth_clone() {
+        let t = TestingStealth::new();
+        assert_eq!(t.inner.counter(), 1);
+        assert!(!t.inner.is_stealth());
+
+        let t_1 = t.stealth_clone();
+        assert_eq!(t.inner.counter(), 1);
+        assert_eq!(t_1.inner.counter(), 1);
+        assert!(!t.inner.is_stealth());
+        assert!(t_1.inner.is_stealth());
+
+        let t_2 = t_1.clone(); // Cloning stealth destructor
+        assert_eq!(t.inner.counter(), 2);
+        assert_eq!(t_1.inner.counter(), 2);
+        assert_eq!(t_2.inner.counter(), 2);
+
+        let t_3 = t.clone(); // Cloning NON-stealth destructor
+        assert_eq!(t.inner.counter(), 3);
+        assert_eq!(t_1.inner.counter(), 3);
+        assert_eq!(t_2.inner.counter(), 3);
+
+        drop(t_1); // Stealth
+        assert_eq!(t.inner.counter(), 3);
+
+        drop(t_2); // Classical
+        assert_eq!(t.inner.counter(), 2);
+
+        drop(t_3); // Classical
+        assert_eq!(t.inner.counter(), 1);
     }
 }
